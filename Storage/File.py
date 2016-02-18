fixed horrible bug in header with offset set to wrong value, still have bug in slotted page

Storage/File.py
@@ -338,10 +338,17 @@ class StorageFile:
     self.file.seek(0)
     data = bytearray(self.file.read())
 
-    fileIndex = (pageId.pageIndex * self.header.pageSize) + self.header.size 
+    fileIndex = self.pageOffset(pageId)
 
-    buffer = data[fileIndex:fileIndex + self.header.pageSize]
-    pageHeader = self.header.pageClass.headerClass.unpack(buffer=buffer)
+    buffer = data[fileIndex:fileIndex + self.pageSize()]
+    # pageHeader = self.header.pageClass.headerClass.unpack(buffer=buffer)
+
+
+    pageHeader = (self.header.pageClass.unpack(pageId=pageId, buffer=buffer)).header
+    # tf = open("tuples.txt", "w")
+    # tf.write(str(pageHeader.numTuples()))
+    # tf.close()
+
     return pageHeader
 
   # Writes a page header to disk.
@@ -357,7 +364,7 @@ class StorageFile:
     self.file.seek(0)
     data = bytearray(self.file.read())
 
-    fileIndex = (page.pageId.pageIndex * self.header.pageSize) + self.header.size 
+    fileIndex = self.pageOffset(page.pageId)
 
     # splices from start of page to size of page header (page header bytes
     #  will always be in front right?) 
@@ -380,19 +387,23 @@ class StorageFile:
     #heapfile = open(self.filePath, "rb")
     #data = bytearray(heapfile.read())
     #heapfile.close()
+
+    #page parameter is a buffer!
+    # but return a page!
+
     self.file.seek(0)
     data = bytearray(self.file.read())
 
-    fileIndex = (pageId.pageIndex * self.header.pageSize) + self.header.size 
+    fileIndex = self.pageOffset(pageId) 
 
-    buffer = data[fileIndex:fileIndex + self.header.pageSize]
+    page = data[fileIndex:fileIndex + self.header.pageSize]
     
-    page = self.header.pageClass.unpack(pageId, buffer)
+    
 
     # # TODO change to page cls not SlottedPage
     # page = SlottedPage.unpack( buffer, pageId)
 
-    return page
+    return self.header.pageClass.unpack(pageId, page)
     # raise NotImplementedError
 
   def writePage(self, page):
@@ -400,7 +411,7 @@ class StorageFile:
     # tf.write(str(page.header.numTuples()))
     # tf.close()
 
-    fileIndex = (page.pageId.pageIndex * self.header.pageSize) + self.header.size 
+    fileIndex = self.pageOffset(page.pageId)
 
 
     #heapfile = open(self.filePath, "rb")
@@ -411,6 +422,11 @@ class StorageFile:
 
     data[fileIndex : fileIndex + self.header.pageSize] = page.pack()
 
+    newPage = self.header.pageClass.unpack(pageId=page.pageId, buffer=page.pack())
+
+    tf = open("numtuples.txt", "w")
+    tf.write(str(page.header.numTuples()) + " , " + str(newPage.header.numTuples()))
+    tf.close()
 
     #heapfile = open(self.filePath, "wb")
     #heapfile.write(data)
@@ -431,9 +447,9 @@ class StorageFile:
     #heapfile.write(page.pack())
     #heapfile.close()
     self.file.seek(0, 2) #should seek to the end of the file
-    data = bytearray(self.file.read())
+    self.file.write(page.pack())
 
-    heapq.heappush(self.freePages, page)
+    self.freePages.append(page)
 
     return pId
 

Storage/Page.py
@@ -128,9 +128,13 @@ class PageHeader:
     self.flags           = kwargs.get("flags", b'\x00')
     self.tupleSize       = kwargs.get("tupleSize", None)
     self.pageCapacity    = kwargs.get("pageCapacity", len(buffer))
-    self.freeSpaceOffset = self.size
+    self.freeSpaceOffset = kwargs.get("freeSpaceOffset", self.size)
 
-    buffer[0:self.size] = self.pack()
+    # tf = open("numtuples.txt", "a")
+    # tf.write(str(page.header.numTuples()) + " , " + str(newPage.header.numTuples()))
+    # tf.close()
+
+    # buffer[0:self.size] = self.pack()
 
   # Page header equality operation based on header fields.
   def __eq__(self, other):
@@ -207,6 +211,7 @@ class PageHeader:
   @classmethod
   def unpack(cls, buffer):
     values = PageHeader.binrepr.unpack_from(buffer)
+
     if len(values) == 4:
       return cls(buffer=buffer, flags=values[0], tupleSize=values[1],
                  freeSpaceOffset=values[2], pageCapacity=values[3])
@@ -469,6 +474,7 @@ class Page(BytesIO):
   def unpack(cls, pageId, buffer):
     
     pageHeader = PageHeader.unpack(buffer)
+
     return Page(pageId=pageId, buffer=buffer, header=pageHeader)
 
     # raise NotImplementedError